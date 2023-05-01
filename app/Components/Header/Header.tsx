a45b775cb8d74ac31b10f47cff0b514ae23c7b65
Jordan <jordanroberts333@icloud.com>
2023-05-01 19:11:29 +0100

    initial commit

---
+++ app/Components/Header/Header.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header className='w-full flex bg-white'>
+        <nav>
+            <ul>
+                <li></li>
+            </ul>
+        </nav>
+        <h2 className='ml-auto'>Curve Learning</h2>
+        <button>Get Started</button>
+    </header>
+  )
+}
+
+export default Header
